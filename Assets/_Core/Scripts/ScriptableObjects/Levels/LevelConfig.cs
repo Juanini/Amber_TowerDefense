3e426e9b2fcdee568cdaab5f34e3b190f3d2a9c9
Juan Vazquez <diclonius32@gmail.com>
2023-11-24 22:35:52 -0600

    * Level Scene connection

--- Assets/_Core/Scripts/ScriptableObjects/Levels/LevelConfig.cs
+++ Assets/_Core/Scripts/ScriptableObjects/Levels/LevelConfig.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelConfig", menuName = "Config/Level Config", order = 1)]
 public class LevelConfig : ScriptableObject
 {
+    [ValueDropdown("GetLevelNames")]
+    [BoxGroup("Scene")] public string sceneName;
+    
     [BoxGroup("Properties")] public int PlayerHealth;
     [BoxGroup("Properties")] public int InitialGold;
     [BoxGroup("Properties")] public int NumberOfWaves;
@@ -13,4 +16,10 @@ public class LevelConfig : ScriptableObject
     [InfoBox("In Milliseconds")]
     [BoxGroup("Enemies")] public int spawnTime = 1500;
     [BoxGroup("Enemies")] public List<EnemyType> enemiesToSpawn;
+    
+    private static readonly ValueDropdownList<string> GetLevelNames = new ValueDropdownList<string>()
+    {
+        { "Level 1", GameConst.LEVEL_1 },
+        { "Level 2", GameConst.LEVEL_2 }
+    };
 }
